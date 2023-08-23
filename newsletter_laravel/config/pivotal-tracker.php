dcd4ad49f0171210a3cbd5203c2a34ae6dcf2bdd
kuderl <126083729+kuderluka@users.noreply.github.com>
2023-08-23 09:20:04 +0200

    env() calls in config file

--- newsletter_laravel/config/pivotal-tracker.php
+++ newsletter_laravel/config/pivotal-tracker.php
@@ -1,6 +1,6 @@
 <?php
     return [
-        'api_token' => '627d82676e1f0a342fb974949da5599c',
-        'project_id' => 2665689,
-        'review_type' => 6614426
+        'api_token' => env('PIVOTAL_API_TOKEN', ''),
+        'project_id' => env('PIVOTAL_PROJECT_ID', ''),
+        'review_type' => env('PIVOTAL_REVIEW_TYPE', ''),
     ];
